9b240887dc3844d057f9c180f5b1fa654058196a
Rustin1 <92025519+Rustin1@users.noreply.github.com>
2023-12-04 16:46:08 +0100

    Add files via upload

---
+++ lab8/src/Rekordy/zad10/TestBankAccount.java
@@ -0,0 +1,11 @@
+package Rekordy.zad10;
+
+public class TestBankAccount {
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount(123,500);
+        BankAccount b2 = new BankAccount(123);
+        System.out.println(b1);
+        System.out.println(b2);
+
+    }
+}
