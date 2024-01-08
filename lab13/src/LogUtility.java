0ee83495401e40d9584c7cfb7a31596b6938aa60
Rustin1 <92025519+Rustin1@users.noreply.github.com>
2024-01-08 15:21:10 +0100

    Wariant 454

---
+++ lab13/src/LogUtility.java
@@ -0,0 +1,5 @@
+public class LogUtility {
+    public static <T> void logPair(T element1, T element2){
+        System.out.println("Element1: "+element1+", Element2: "+element2);
+    }
+}
---
+++ lab13/src/TestLogUtility.java
@@ -0,0 +1,6 @@
+
+public class TestLogUtility {
+    public static void main(String[] args) {
+        LogUtility.logPair("Hello","World");
+    }
+}
